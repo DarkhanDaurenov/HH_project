276a73029019f77e86a96e2ba34145c96de8cb80
DarkhanDaurenov <takishevdarkhan@gmail.com>
2024-03-08 22:52:22 +0600

    add src/main.py

--- src/main.py
+++ src/main.py
@@ -1,7 +1,10 @@
-from src.hhabc import (HeadHunterAPI)
-from src.vacancyHh import Vacancy
+from src.hhabc import HeadHunterAPI
+from src.vacancyHh import Vacancy, JSONSaver
+from typing import List
 
-def user_interaction():
+
+def user_interaction() -> None:
+    """Функция для взаимодействия с пользователем."""
     hh_api = HeadHunterAPI()
     search_query = input("Введите поисковый запрос: ")
     hh_vacancies = hh_api.get_vacancies(search_query)
@@ -13,6 +16,18 @@ def user_interaction():
     top_vacancies = sorted_vacancies[:top_n]
     for vacancy in top_vacancies:
         print(vacancy.title, vacancy.salary)
+
+    # Сохранение вакансий в JSON файл
+    json_saver = JSONSaver()
+    for vacancy in vacancies_list:
+        json_saver.add_vacancy(vars(vacancy))
+
+    # Фильтрация вакансий
+    filter_criteria = input("Введите критерий для фильтрации вакансий: ")
+    filtered_vacancies = json_saver.filter_vacancies(filter_criteria)
+    print("Фильтрованные вакансии:")
+    for vacancy in filtered_vacancies:
+        print(vacancy)
 
 
 if __name__ == "__main__":

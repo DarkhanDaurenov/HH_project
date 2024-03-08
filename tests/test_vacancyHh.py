2cd91df19822c3416147c18e28ed963a354eec8d
DarkhanDaurenov <takishevdarkhan@gmail.com>
2024-03-08 22:53:23 +0600

    add tests/test_vacancyHh.py

--- tests/test_vacancyHh.py
+++ tests/test_vacancyHh.py
@@ -9,16 +9,16 @@ def json_saver(tmp_path):
 
 def test_add_vacancy(json_saver):
     vacancy = Vacancy("Python Developer", "", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
-    json_saver.add_vacancy(vacancy)
+    json_saver.add_vacancy(vars(vacancy))  # Представляем объект в виде словаря
     with open(json_saver.filename, "r") as f:
         data = json.load(f)
     assert len(data) == 1
-    assert data[0]["title"] == "Python Developer"
+    assert data[0]["title"] == vacancy.title
 
 def test_delete_vacancy(json_saver):
     vacancy = Vacancy("Python Developer", "", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
-    json_saver.add_vacancy(vacancy)
-    json_saver.delete_vacancy(vacancy)
+    json_saver.add_vacancy(vars(vacancy))  # Представляем объект в виде словаря
+    json_saver.delete_vacancy(vars(vacancy))  # Представляем объект в виде словаря
     with open(json_saver.filename, "r") as f:
         data = json.load(f)
     assert len(data) == 0
@@ -26,10 +26,10 @@ def test_delete_vacancy(json_saver):
 def test_add_and_delete_vacancy(json_saver):
     vacancy1 = Vacancy("Python Developer", "", "100 000-150 000 руб.", "Требования: опыт работы от 3 лет...")
     vacancy2 = Vacancy("Java Developer", "", "90 000-120 000 руб.", "Требования: опыт работы от 2 лет...")
-    json_saver.add_vacancy(vacancy1)
-    json_saver.add_vacancy(vacancy2)
+    json_saver.add_vacancy(vars(vacancy1))  # Представляем объект в виде словаря
+    json_saver.add_vacancy(vars(vacancy2))  # Представляем объект в виде словаря
     assert len(json_saver.vacancies) == 2
-    json_saver.delete_vacancy(vacancy1)
+    json_saver.delete_vacancy(vars(vacancy1))  # Представляем объект в виде словаря
     assert len(json_saver.vacancies) == 1
-    json_saver.delete_vacancy(vacancy2)
+    json_saver.delete_vacancy(vars(vacancy2))  # Представляем объект в виде словаря
     assert len(json_saver.vacancies) == 0
